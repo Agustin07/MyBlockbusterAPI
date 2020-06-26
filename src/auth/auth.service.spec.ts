8ebcb4aa25c7351292bd02ebec5a561a11affe32
Agus <rigoomartinez@gmail.com>
2020-06-26 12:22:05 -0600

    Change a unit test for auth services

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,21 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
-import { UserDto } from '../users/dto/user.dto';
 import UsersService from '../users/users.service';
-import { user1 } from '../users/mocks/users.mocks';
+import { user1, userDTo1 } from '../users/mocks/users.mocks';
 import { UnauthorizedException } from '@nestjs/common';
 
 const mock_UsersService = {
   findOneByEmail: jest.fn(),
 };
 const mock_jwtService = {
-  findOneByEmail: jest.fn(),
+  sign: jest.fn(),
 };
 
 describe('AuthService', () => {
   let service: AuthService;
   let usersService: UsersService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,6 +26,7 @@ describe('AuthService', () => {
       ],
     }).compile();
     usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
     service = module.get<AuthService>(AuthService);
   });
 
@@ -47,7 +48,7 @@ describe('AuthService', () => {
       expect(await service.validateUser(user.email, 'pineapple')).toBe(user);
       expect(spyfindOneByEmail).toHaveBeenCalledTimes(1);
     });
-    it('should throw on invalid email', () => {
+    it('should throw on invalid email', async () => {
       const user = user1;
       const spyfindOneByEmail = jest
         .spyOn(usersService, 'findOneByEmail')
@@ -55,7 +56,7 @@ describe('AuthService', () => {
           return Promise.resolve(undefined);
         });
       try {
-        service.validateUser('jsoidfjsdm', 'pineapple');
+        await service.validateUser('jsoidfjsdm', 'pineapple');
       } catch (error) {
         expect(error).toBeInstanceOf(UnauthorizedException);
       }
@@ -65,6 +66,14 @@ describe('AuthService', () => {
   describe('login', () => {
     it('should be defined', () => {
       expect(service.login).toBeDefined();
+    });
+    it('should return {access_token: jwtToken}', async () => {
+      const ImAToken = '$hklknhmlhKUG6D8.onhIGUKhnGKMhg';
+      const userDto = userDTo1;
+      const spysign = jest.spyOn(jwtService,'sign').mockImplementation(()=>ImAToken);
+      const expected = {access_token: '$hklknhmlhKUG6D8.onhIGUKhnGKMhg'};
+      expect(await service.login(userDto)).toEqual(expected);
+      expect(spysign).toHaveBeenCalled();
     });
   });
 
